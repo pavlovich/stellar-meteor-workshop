555ddde6417d2788dd7cf48e013877ad3433e176
Peter Pavlovich <pavlovich@gmail.com>
2016-09-25 19:32:16 -0400

    Use server-side security.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.methods({
+  addTask: function (task) {
+    Tasks.validateInsert(Meteor.userId(), task);
+    Tasks.insert(task);
+  },
+  updateTask: function (task) {
+    var id = task._id;
+    Tasks.validateUpdate(Meteor.userId(), {$set: task});
+    Tasks.update(id, {$set: task});
+  },
+  deleteTask: function (doc) {
+    Tasks.validateDelete(Meteor.userId(), doc);
+    Tasks.remove(doc._id);
+  },
+  upsertTask: function (task) {
+    var id = task._id;
+    if (id) {
+      Tasks.validateUpdate(Meteor.userId(), {$set: task});
+    } else {
+      Tasks.validateInsert(Meteor.userId(), task);
+    }
+    Tasks.upsert(id, task);
+  }
 });
